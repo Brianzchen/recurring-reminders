6b05d4344899ca0ecbecbf3d85b87857876395b7
Brian Chen <bzchen93@gmail.com>
2018-04-29 11:18:03 +1200

    can store reminder

--- src/service/addReminder.js
+++ src/service/addReminder.js
@@ -1,19 +1,21 @@
-// name - The name of the reminder
-// days - Which day of the week the reminder should trigger
-// frequency - how often the reminder should trigger on a week to week basis
+// name (string) - The name of the reminder
+// days (number) - Which day of the week the reminder should trigger
+//    0 = Su, 1 = M, 2 = Tu, 3 = W, 4 = Th, 5 = F, 6 = Sa
+// frequency (number) - how often the reminder should trigger on a week to week basis
 //    where frequency is the number of weeks
 
 const remindersKey = 'recurringReminders';
 
 const createNewReminder = reminder => ({
+  uid: `${new Date().getTime()}`,
   name: reminder.name,
   days: reminder.days,
-
+  frequency: reminder.frequency,
+  last: new Date().getTime(),
 });
 
 export default reminder => new Promise(resolve => {
   const storedReminders = JSON.parse(localStorage.getItem(remindersKey));
-  console.log(storedReminders);
 
   const reminders = [];
   if (storedReminders !== null) {
