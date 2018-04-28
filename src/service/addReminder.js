11ab739dd1b7827e6c8a664a0346e5565822d812
Brian Chen <bzchen93@gmail.com>
2018-04-29 11:23:09 +1200

    create getter method from serice

--- src/service/addReminder.js
+++ src/service/addReminder.js
@@ -1,10 +1,11 @@
+import remindersKey from './remindersKey';
+import getReminders from './getReminders';
+
 // name (string) - The name of the reminder
 // days (number) - Which day of the week the reminder should trigger
 //    0 = Su, 1 = M, 2 = Tu, 3 = W, 4 = Th, 5 = F, 6 = Sa
 // frequency (number) - how often the reminder should trigger on a week to week basis
 //    where frequency is the number of weeks
-
-const remindersKey = 'recurringReminders';
 
 const createNewReminder = reminder => ({
   uid: `${new Date().getTime()}`,
@@ -15,18 +16,14 @@ const createNewReminder = reminder => ({
 });
 
 export default reminder => new Promise(resolve => {
-  const storedReminders = JSON.parse(localStorage.getItem(remindersKey));
-
-  const reminders = [];
-  if (storedReminders !== null) {
-    reminders.push(storedReminders);
-  }
-  reminders.push(createNewReminder(reminder));
-
-  localStorage.setItem(
-    remindersKey,
-    JSON.stringify(reminders),
-  );
-
-  resolve();
+  getReminders().then(storedReminders => {
+    localStorage.setItem(
+      remindersKey,
+      JSON.stringify([
+        ...storedReminders,
+        createNewReminder(reminder),
+      ]),
+    );
+    resolve();
+  });
 });
