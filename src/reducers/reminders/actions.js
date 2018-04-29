944fc2563c48c9035f0d86b0349ded4c797951c2
Brian Chen <bzchen93@gmail.com>
2018-04-29 15:05:12 +1200

    restructure get reminders redux logic to move distribution to reducer

--- src/reducers/reminders/actions.js
+++ src/reducers/reminders/actions.js
@@ -3,19 +3,10 @@ import * as constants from './constants';
 export const getReminders = () => (
   (dispatch, getState, { service }) => {
     service.getReminders().then(reminders => {
-      const outstanding = [];
-      const upcoming = [];
-
-      for (let i = 0, len = reminders.length; i < len; i++) {
-        const reminder = reminders[i];
-        outstanding.push(reminder);
-        upcoming.push(reminder);
-      }
-
       dispatch({
-        type: constants.SET_UPCOMING_REMINDERS,
+        type: constants.SET_REMINDERS,
         payload: {
-          reminders: upcoming,
+          reminders,
         },
       });
     });
--- src/reducers/reminders/index.js
+++ src/reducers/reminders/index.js
@@ -3,10 +3,11 @@ import initialState from './initialState';
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case constants.SET_UPCOMING_REMINDERS:
+    case constants.SET_REMINDERS:
       return {
         ...state,
         upcoming: action.payload.reminders,
+        outstanding: action.payload.reminders,
       };
     case constants.DELETE_REMINDER:
       return {
