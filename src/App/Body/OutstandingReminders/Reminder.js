61d6ad5d0903180238e37935c85a7502bcaeec99
Brian Chen <bzchen93@gmail.com>
2018-04-29 15:14:49 +1200

    basics of outstanding reminds

--- src/App/Body/OutstandingReminders/Reminder.js
+++ src/App/Body/OutstandingReminders/Reminder.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { StyleSheet, css } from 'aphrodite';
+
+import Icon from 'components/Icon';
+
+const styles = StyleSheet.create({
+  container: {
+    cursor: 'pointer',
+  },
+});
 
 const Reminder = props => (
-  <div>
+  <div className={css(styles.container)}>
+    <Icon
+      icon="checkbox-blank-outline"
+    />
     {props.name}
   </div>
 );
+
+Reminder.propTypes = {
+  name: PropTypes.string.isRequired,
+};
 
 export default Reminder;
--- src/App/Body/OutstandingReminders/index.js
+++ src/App/Body/OutstandingReminders/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { StyleSheet, css } from 'aphrodite';
+import { map } from 'lodash';
 
 import SectionHeader from 'components/SectionHeader';
+
+import Reminder from './Reminder';
 
 const styles = StyleSheet.create({
   container: {
@@ -9,10 +14,26 @@ const styles = StyleSheet.create({
   },
 });
 
-const OutstandingReminders = () => (
+const OutstandingReminders = props => (
   <div className={css(styles.container)}>
     <SectionHeader value="Outstanding" />
+    {
+      map(props.reminders, o => (
+        <Reminder
+          key={o.uid}
+          {...o}
+        />
+      ))
+    }
   </div>
 );
 
-export default OutstandingReminders;
+OutstandingReminders.propTypes = {
+  reminders: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+const mapStateToProps = state => ({
+  reminders: state.reminders.outstanding,
+});
+
+export default connect(mapStateToProps)(OutstandingReminders);
