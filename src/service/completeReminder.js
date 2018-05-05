b07c38f17470bdb3c8ef1739d9def5e333dd6682
Brian Chen <bzchen93@gmail.com>
2018-05-05 17:23:52 +1200

    can uncomplete reminder and completing will increment by 3 days as for testing

--- src/service/completeReminder.js
+++ src/service/completeReminder.js
@@ -1,12 +1,30 @@
-import { find } from 'lodash';
+import { find, map } from 'lodash';
+
+import { weekMilli } from 'lib/time';
 
 import getReminders from './getReminders';
+import remindersKey from './remindersKey';
+
+const getNext = reminder => {
+  return reminder.next + weekMilli;
+};
 
 export default uid => new Promise(resolve => {
   getReminders().then(reminders => {
     const reminder = find(reminders, { uid });
 
-
+    localStorage.setItem(
+      remindersKey,
+      JSON.stringify(map(reminders, o => {
+        if (o.uid === uid) {
+          return {
+            ...o,
+            next: getNext(reminder),
+          };
+        }
+        return o;
+      })),
+    );
     resolve(reminder);
   });
 });
--- src/service/uncompleteReminder.js
+++ src/service/uncompleteReminder.js
@@ -1,11 +1,24 @@
-import { find } from 'lodash';
+import { find, map } from 'lodash';
 
 import getReminders from './getReminders';
+import remindersKey from './remindersKey';
 
 export default (uid, previous) => new Promise(resolve => {
   getReminders().then(reminders => {
     const reminder = find(reminders, { uid });
 
+    localStorage.setItem(
+      remindersKey,
+      JSON.stringify(map(reminders, o => {
+        if (o.uid === uid) {
+          return {
+            ...o,
+            next: previous,
+          };
+        }
+        return o;
+      })),
+    );
     resolve(reminder);
   });
 });
