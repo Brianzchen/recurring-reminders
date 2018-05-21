ff333e69df30740a082b5dccd8dff384e9069b8c
Brian <bchen.chen93@gmail.com>
2018-05-21 22:04:05 +1000

    create todo reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,9 +3,11 @@ import { routerReducer } from 'react-router-redux';
 
 import note from './note';
 import reminders from './reminders';
+import todo from './todo';
 
 export default combineReducers({
   note,
   reminders,
   router: routerReducer,
+  todo,
 });
