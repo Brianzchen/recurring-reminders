db52d4e0da11b018453c6547a74d2d226f25e28a
Brian <bchen.chen93@gmail.com>
2018-05-22 20:49:28 +1000

    added styles to todo so it doesn't overflow

--- src/App/Body/Todo/index.js
+++ src/App/Body/Todo/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { StyleSheet, css } from 'aphrodite';
 import { map } from 'lodash';
 
 import SectionHeader from 'components/SectionHeader';
@@ -8,17 +9,31 @@ import SectionHeader from 'components/SectionHeader';
 import NewTodo from './NewTodo';
 import TodoItem from './TodoItem';
 
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    maxHeight: '50%',
+  },
+  list: {
+    flex: 1,
+    overflow: 'auto',
+  },
+});
+
 const Todo = props => (
-  <div>
+  <div className={css(styles.container)}>
     <SectionHeader value="Todo" />
-    {
-      map(props.todos, o => (
-        <TodoItem
-          key={o.uid}
-          {...o}
-        />
-      ))
-    }
+    <div className={css(styles.list)}>
+      {
+        map(props.todos, o => (
+          <TodoItem
+            key={o.uid}
+            {...o}
+          />
+        ))
+      }
+    </div>
     <NewTodo />
   </div>
 );
