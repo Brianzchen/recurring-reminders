a4873f76f290bc8e8efc612ce3338ff34171f778
Brian <bzchen93@gmail.com>
2018-06-26 21:48:38 +1000

    add transition to overlay falling down

--- src/App/Hide/Overlay/index.js
+++ src/App/Hide/Overlay/index.js
@@ -7,9 +7,26 @@ import Body from './Body';
 import Notification from './Notification';
 import Show from './Show';
 
+const dropDownKeyFrame = {
+  '0%': {
+    top: '-100%',
+  },
+  '75%': {
+    top: '-7%',
+  },
+  '80%': {
+    top: '0',
+  },
+  '90%': {
+    top: '-5%',
+  },
+};
+
 const styles = StyleSheet.create({
   background: {
     flexDirection: 'column',
+    animationName: [dropDownKeyFrame],
+    animationDuration: '1s',
   },
 });
 
