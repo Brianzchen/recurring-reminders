e9dd10d586a2de0d9146ac6ae80e3dedf35a5769
Brian <bzchen93@gmail.com>
2018-06-27 23:22:02 +1000

    build notification

--- src/App/Hide/Overlay/Notification.js
+++ src/App/Hide/Overlay/Notification.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { StyleSheet, css } from 'aphrodite';
+
+import { secondary, alternate } from 'lib/colors';
+
+const styles = StyleSheet.create({
+  container: {
+    padding: '8px',
+    borderRadius: '5px',
+    backgroundColor: secondary,
+    boxShadow: `0 0 10px 5px ${alternate}`,
+  },
+});
 
 const Notification = () => (
-  <div />
+  <div
+    className={css(styles.container)}
+  >
+    1 active reminder
+  </div>
 );
 
 export default Notification;
